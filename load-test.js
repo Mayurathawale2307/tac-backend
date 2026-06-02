db0ff561c666bb5ce15f10afeffd09bacb3b29bc
Mayur <athawalemayur115@gmail.com>
2026-06-02 17:57:56 +0530

    add health route

--- load-test.js
+++ load-test.js
@@ -1,13 +1,15 @@
 import http from "k6/http";
 import { check } from "k6";
 
+export const options = {
+  vus: 10,
+  duration: "10s",
+};
+
 export default function () {
   const res = http.get(
     "https://tac-backend-erf1.onrender.com/api/health"
   );
-
-  console.log("STATUS:", res.status);
-  console.log("BODY:", res.body);
 
   check(res, {
     "status is 200": (r) => r.status === 200,
